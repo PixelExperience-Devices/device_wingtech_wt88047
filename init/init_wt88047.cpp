764b755ab5ec97f5e81283cb984dc3eaba0309a8
MkfsSion <myychina28759@gmail.com>
2018-09-26 08:44:05 +0800

    wt88047: Set Android Go properties for 1 GB memory device
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -154,4 +154,12 @@ void init_target_properties()
     property_set("dalvik.vm.heaptargetutilization", "0.75");
     property_set("dalvik.vm.heapminfree", "512k");
     property_set("dalvik.vm.heapmaxfree", "8m");
+    if (!is2GB()) {
+	    property_set("ro.config.low_ram", "true");
+	    property_set("ro.lmk.critical_upgrade", "true");
+	    property_set("ro.lmk.upgrade_pressure", "40");
+	    property_set("pm.dexopt.downgrade_after_inactive_days", "10");
+	    property_set("pm.dexopt.shared", "quicken");
+	    property_override("dalvik.vm.heapgrowthlimit", "128m");
+    }
 }
